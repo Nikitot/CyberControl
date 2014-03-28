bd30999348e2aaeb61a4094306f625e55d343ce7
Nikitot <x.mister@bk.ru>
2014-03-28 19:15:19 +0400

    first commit, .gitignir

---
+++ CyberControl_4/CapturesMatching.cpp
@@ -0,0 +1,75 @@
+#include "stdafx.h"
+#include "CapturesMatching.h"
+#pragma warning (disable 4244)
+
+int matrix[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+CvCapture *CapturesMatching::cameraFrame(int number){
+	CvCapture *capture = 0;
+	capture = cvCreateCameraCapture(number);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 640);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 480);
+	return capture;
+}
+
+IplImage *CapturesMatching::createRemap(CvSize size){
+	IplImage *map = cvCreateImage(size, IPL_DEPTH_32F, 1);
+	return map;
+}
+
+void CapturesMatching::openMatrix(char *path){
+	ifstream file;
+	file.open(path);				//"C:\\matrix.txt"
+	if (!file){
+		cout << "Error: оpening matrix perspective correction a failure" << endl;
+		return;
+	}
+	else cout << "Opening matrix perspective correction successfully completed" << endl;
+
+	int cit = 0;
+	while (!file.eof()){
+		file >> matrix[cit];
+		cout << matrix[cit] << "\t";
+		switch (cit){
+			case 1: cout << ": Top left" << endl;
+				break;
+			case 3: cout << ": Top right" << endl;
+				break;
+			case 5: cout << ": Bottom left" << endl;
+				break;
+			case 7: cout << ": Bottom right" << endl;
+				return;
+		}
+		cit++;
+	}
+}
+
+//корректируем перспективу первого изображения в соответствии со вторым
+void CapturesMatching::correctivePerspective(IplImage *frame0){
+	int width = frame0->width;
+	int height = frame0->height;
+
+	// точки
+	CvPoint2D32f srcQuad[4], dstQuad[4];
+
+	// матрица преобразования
+	CvMat* warp_matrix = cvCreateMat(3, 3, CV_32FC1);
+
+	// задаём точки
+	srcQuad[0].x = 0;			srcQuad[0].y = 0;					//src Top left
+	srcQuad[1].x = width - 1;	srcQuad[1].y = 0;					//src Top right
+	srcQuad[2].x = 0;			srcQuad[2].y = height - 1;			//src Bottom left
+	srcQuad[3].x = width - 1;	srcQuad[3].y = height - 1;			//src Bot right
+	//- - - - - - - - - - - - - -//
+	dstQuad[0].x = -matrix[0];			dstQuad[0].y = matrix[1];				//dst Top left       
+	dstQuad[1].x = width + matrix[2];	dstQuad[1].y = matrix[3];				//dst Top right        
+	dstQuad[2].x = -matrix[4];			dstQuad[2].y = height + matrix[5];		//dst Bot left          
+	dstQuad[3].x = width + matrix[6];	dstQuad[3].y = height + matrix[7];		//dst Bot right
+
+	// получаем матрицу преобразования
+	cvGetPerspectiveTransform(srcQuad, dstQuad, warp_matrix);
+	// преобразование перспективы
+	cvWarpPerspective(frame0, frame0, warp_matrix);
+}
+
+
---
+++ CyberControl_4/stdafx.h
@@ -0,0 +1,12 @@
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+
+using namespace std;
+using namespace cv;
